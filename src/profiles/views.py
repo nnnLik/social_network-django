97e80097c58dda20f4d4ee1d7885fe644d405bc3
nnnLik <makhmudov_ra@vk.com>
2022-12-22 23:48:56 +0300

    [ADD] update and get user

--- src/profiles/views.py
+++ src/profiles/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework.generics import RetrieveAPIView, UpdateAPIView
+from rest_framework import permissions
 
-# Create your views here.
+from .models import UserSonet
+from .serializers import GetUserSonetSerializer
+
+
+class GetUserSonetView(RetrieveAPIView):
+	"""Display user information"""
+
+	queryset = UserSonet.objects.all()
+	serializer_class = GetUserSonetSerializer
+
+
+class UpdateUserSonetView(UpdateAPIView):
+	"""Edit user information"""
+
+	serializer_class = GetUserSonetSerializer
+	permission_classes = [permissions.IsAuthenticated]
+
+	def get_queryset(self):
+		return UserSonet.objects.filter(id=self.request.user.id)
