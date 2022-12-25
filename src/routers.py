78c767db7ae559bd5117577fcd85863bd7bd2c33
nnnLik <makhmudov_ra@vk.com>
2022-12-25 19:27:32 +0300

    [ADD] swagger

--- src/routers.py
+++ src/routers.py
@@ -3,20 +3,21 @@ from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
-
-# schema_view = get_schema_view(
-#    openapi.Info(title="SoNet API", default_version='v1', description="Docs"),
-#     permission_classes=permissions.AllowAny,
-#     public=True,
-#
-# )
-
+schema_view = get_schema_view(
+	openapi.Info(
+		title="SoNet API",
+		default_version='v1',
+		description="Test description",
+	),
+	public=True,
+	permission_classes=[permissions.AllowAny],
+)
 
 urlpatterns = [
-    path('', include('src.profiles.urls')),
+	path('', include('src.profiles.urls')),
 
-    # path('swagger(?P<format>\.json|\.yaml)', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    # path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    # path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+	path('swagger(?P<format>\.json|\.yaml)', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+	path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+	path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 
 ]
