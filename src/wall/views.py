b4c4de3c7389f4f2b689587fabd2d66868995d8f
nnnLik <makhmudov_ra@vk.com>
2022-12-28 07:43:42 -0800

    [ADD] crud views

--- src/wall/views.py
+++ src/wall/views.py
@@ -1,3 +1,44 @@
-from django.shortcuts import render
+from rest_framework import permissions, generics
 
-# Create your views here.
+from ..viewset.classes import CreateUpdateDestroy, CreateRetrieveUpdateDestroy
+from ..viewset.permissions import IsAuthor
+from .models import Post, Comment
+from .serializers import (PostSerializer, ListPostSerializer, CreateCommentSerializer)
+
+
+class PostListView(generics.ListAPIView):
+    """All user posts"""
+    serializer_class = ListPostSerializer
+
+    def get_queryset(self):
+        return Post.objects.filter(
+            user_id=self.kwargs.get('pk')).select_related('user').prefetch_related('comments')
+
+
+class PostView(CreateRetrieveUpdateDestroy):
+    """ CRUDs post"""
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Post.objects.all().select_related('user').prefetch_related('comments')
+    serializer_class = PostSerializer
+    permission_classes_by_action = {'get': [permissions.AllowAny],
+                                    'update': [IsAuthor],
+                                    'destroy': [IsAuthor]}
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+
+class CommentsView(CreateUpdateDestroy):
+    """ CRUDs comment of posts"""
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Comment.objects.all()
+    serializer_class = CreateCommentSerializer
+    permission_classes_by_action = {'update': [IsAuthor],
+                                    'destroy': [IsAuthor]}
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+    def perform_destroy(self, instance):
+        instance.deleted = True
+        instance.save()
