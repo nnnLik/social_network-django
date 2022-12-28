bfdbe46f8adce58dd5e85ef9d461656481885592
nnnLik <makhmudov_ra@vk.com>
2022-12-28 07:41:43 -0800

    [ADD] common settings

---
+++ src/viewset/serializers.py
@@ -0,0 +1,24 @@
+from rest_framework import serializers
+
+
+class FilterCommentListSerializer(serializers.ListSerializers):
+    """Filter comments"""
+
+    def to_representation(self, data):
+        """
+        Return a list of comments.
+
+        :param data: List of comments
+        :return: List of comments
+        """
+        data = data.filter(parent=None)
+        return super().to_representation(data)
+
+
+class RecursiveFilterCommentSerializer(serializers.Serializer):
+    """Recursive filter comment serializer"""
+
+    def to_representation(self, instance):
+        serializers = self.parent.parent.__class__(instance, context=self.context)
+        return serializers.data
+    
