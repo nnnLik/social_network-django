617de3376d9a086c5303b98aafeaaedcb593568a
nnnLik <makhmudov_ra@vk.com>
2022-12-30 05:18:00 -0800

    [EDIT] http method

--- src/profiles/urls.py
+++ src/profiles/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('profile/<int:pk>/', views.UserSonetView.as_view({'get': 'retrieve', 'put': 'update'})),
-    path('<int:pk>/', views.UserSonetPublicView.as_view({'get': 'retrieve'})),
+    path('profile/<int:pk>/', views.UserSonetView.as_view({
+        'get': 'retrieve','put': 'update'})),
+    path('<int:pk>/', views.UserSonetPublicView.as_view(
+        {'get': 'retrieve'})),
 ]
