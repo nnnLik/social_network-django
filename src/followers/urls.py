f10e40507110efe5a772aae4f7f195e4e3625d53
nnnLik <makhmudov_ra@vk.com>
2023-01-03 12:09:19 -0800

    [ADD] followers path

--- src/followers/urls.py
+++ src/followers/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    path('<int:pk>', views.FollowerView.as_view()),
+    path('<int:pk>', views.AddFollowerView.as_view()),
     path('', views.ListFollowerView.as_view())
 ]
