18a6b6286bb494e7a39ca8fffeaa560f894d4e40
nnnLik <makhmudov_ra@vk.com>
2023-01-07 18:24:36 +0300

    [ADD] related name

--- src/followers/models.py
+++ src/followers/models.py
@@ -1,12 +1,11 @@
 from django.db import models
 from django.conf import settings
-from src.profiles.serializers import UserFollowerSerializer
 
 
 class Follower(models.Model):
     """Follower model"""
 
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE,
-                            related_name='Follower')
+                            related_name='owner')
     subscriber = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE,
-                            related_name='Followers')
+                            related_name='subscribers')
