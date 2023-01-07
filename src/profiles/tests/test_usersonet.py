cfc48d43b192bca83eb2ca9219174ce908395531
nnnLik <makhmudov_ra@vk.com>
2023-01-07 18:19:51 +0300

    [ADD] test for user model

---
+++ src/profiles/tests/test_usersonet.py
@@ -0,0 +1,15 @@
+from django.test import TestCase
+
+from ..models import UserSonet
+
+class UserSonetModelTest(TestCase):
+    """Test custom user model"""
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_create_usersonet(self):
+        pass
