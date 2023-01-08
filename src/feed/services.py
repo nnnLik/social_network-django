08a1bf2adda9e68e3606c22e7ecde1a63bc71d4a
nnnLik <makhmudov_ra@vk.com>
2023-01-08 05:53:13 +0300

    [FIX] get lists of post

--- src/feed/services.py
+++ src/feed/services.py
@@ -6,7 +6,7 @@ from src.wall.models import Post
 class Feed:
     """Service feeds"""
 
-    def     get_post_list(self, user: settings.AUTH_USER_MODEL):
+    def get_post_list(self, user: settings.AUTH_USER_MODEL):
         return Post.objects.filter(user__owner__subscriber_id=1).order_by('-created_date').select_related('user').prefetch_related('comments')
 
     def get_single_post(self, pk: int):
