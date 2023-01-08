c53c2b58b8b607a3c286c6fb4659d5b0bec17199
nnnLik <makhmudov_ra@vk.com>
2023-01-08 05:53:51 +0300

    [ADD] feed view

--- src/feed/views.py
+++ src/feed/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions, viewsets, response
 
-# Create your views here.
+from src.viewset.classes import MixedPermission
+from src.wall.serializers import ListPostSerializer, PostSerializer
+
+from .services import feed_service
+
+
+class FeedView(viewsets.ReadOnlyModelViewSet):
+    """A feed view"""
+    
+    permission_classes = [permissions.IsAuthenticated]
+    serializer_class = ListPostSerializer
+
+    
+    def list(self, request, *args, **kwargs):
+        queryset = feed_service.get_post_list(request.user)
+        serializer = self.get_serializer(queryset, many=True)
+        print('[INFO] Posts was collected successfully')
+        return response.Response(serializer.data)
+
+    def retrieve(self, request, *args, **kwargs):
+        instance = feed_service.get_single_post(kwargs.get('pk'))
+        serializer = PostSerializer(instance)
+        return response.Response(serializer.data)
